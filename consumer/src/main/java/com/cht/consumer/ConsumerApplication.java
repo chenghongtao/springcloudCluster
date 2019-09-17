f503a4b27335949f3557b16eda8671afe513366c
chenghongtao <18729553226@163.com>
2019-09-17 15:09:51 +0800

    可视化数据监控-dashboard

--- consumer/src/main/java/com/cht/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/cht/consumer/ConsumerApplication.java
@@ -2,11 +2,16 @@ package com.cht.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 import feign.Logger;
 
@@ -15,15 +20,28 @@ import feign.Logger;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableCaching
+
+@EnableHystrix
+@EnableHystrixDashboard
 public class ConsumerApplication {
-	
+
 	@Bean
-	public Logger.Level getLog(){
+	public Logger.Level getLog() {
 		return Logger.Level.FULL;
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
 	}
 
 }
